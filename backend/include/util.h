151ddd79c2e9dafc5b8dd40523870545e40eea68
juliagabriellafs <juliagabriellaf0407@gmail.com>
2026-06-07 01:27:32 -0300

    feat/insere função para converter o formato de horas em minutos

--- backend/include/util.h
+++ backend/include/util.h
@@ -5,6 +5,7 @@
 #include "local.h"
 
 int strings_iguais_case_insensitive(const char *a, const char *b);
+int converter_horario_em_minutos(const char *horario_str, int *inicio_min, int *fim_min);
 void imprimir_local(const Local *local);
 void imprimir_resultado(const ResultadoBusca *resultado, const Local *locais);
 
